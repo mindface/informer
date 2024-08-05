88fa5e9e911204ac360ed46cd15cbcfad5961027
mindface <in00und@gmail.com>
2024-08-05 11:20:17 +0900

    first commit

---
+++ src/content/config.ts
@@ -0,0 +1,16 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+	type: 'content',
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		mainImage: z.string().optional(),
+	}),
+});
+
+export const collections = { blog };
+
+console.log(collections);
